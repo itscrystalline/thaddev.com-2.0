77efb19443a3ce8d21cc9fe74453f310f6c67080
MyNameTsThad <pvpthadgaming@gmail.com>
2023-12-10 18:55:42 +0700

    allow x frame options

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
 import '../styles/globals.css'
 import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <>
-    <Head>
-      <meta name="viewport" content="initial-scale=1, width=device-width"/>
-      <meta content="ThadDev • Home" property="og:title"/>
-      <meta content="ThadDev (IWant2TryHard)'s place on the internet." property="og:description"/>
-      <meta content="https://thaddev.com" property="og:url"/>
-      <meta content="https://thaddev.com/images/logoshort.svg" property="og:image"/>
-      <meta content="#FFAA00" data-react-helmet="true" name="theme-color"/>
-    </Head>
-    <Component {...pageProps} />
-  </>
+function MyApp({Component, pageProps}) {
+    return <>
+        <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            <meta content="ThadDev • Home" property="og:title"/>
+            <meta content="ThadDev (IWant2TryHard)'s place on the internet." property="og:description"/>
+            <meta content="https://thaddev.com" property="og:url"/>
+            <meta content="https://thaddev.com/images/logoshort.svg" property="og:image"/>
+            <meta content="#FFAA00" data-react-helmet="true" name="theme-color"/>
+            <meta httpEquiv="X-FRAME-OPTIONS" content="SAMEORIGIN"/>
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
